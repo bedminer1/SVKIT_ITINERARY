dbdba2f9d8e14b371e83f77410b2f6e481000d5e
bedminer1 <alexander.seah.min.rui@gmail.com>
2023-11-08 20:47:28 +0800

    Set up the routes

---
+++ tailwindcss_practice/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+  </script>
+  <div class="flex items-center w-full h-10 font-serif text-white bg-black gap-9 justify-left">
+      <p class="w-12 h-6 py-1 pl-9"><a href="./">Home</a></p>
+      <p class="w-12 h-6 py-1 pl-9"><a href="./food">Food</a></p>
+      <p class="w-12 h-6 py-1 pl-9"><a href="./culture">Culture</a></p>
+      <p class="w-12 h-6 py-1 pl-9"><a href="./shopping">Shopping</a></p>
+  </div>
+  <slot />
---
+++ tailwindcss_practice/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+  
+<div class="flex items-center justify-center w-full h-screen">
+  <h1 class="text-3xl text-center text-slate-950">
+    Hello world!
+  </h1>
+  <p>
+    
+  </p>
+</div>
+  
